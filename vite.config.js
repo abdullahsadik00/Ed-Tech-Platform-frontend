65b101bd99d84bd59e5d05b34e0b62bc7d965274
Sadik Shaikh <abdullahsadik00@gmail.com>
2025-01-24 21:22:00 +0530

    updated package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: 'esnext', // Ensures modern JS output
+    outDir: 'dist', // Default build directory
+    modulePreload: true, // Adds <link rel="modulepreload"> for module scripts
+  },
+});
