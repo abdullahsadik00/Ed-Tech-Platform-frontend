23237437d19874ff22e64490b614e77ab2f92241
sadik.shaikh <sadik.shaikh@saralux.com>
2025-04-09 10:36:56 +0530

    added the dummy component for student dashboard overview component

--- src/pages/dashboard/components/Overview.tsx
+++ src/pages/dashboard/components/Overview.tsx
@@ -1,4 +1,9 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import WelcomeBanner from "./WelcomeBanner"
+import { ProgressCard } from "./ProgressCard"
+import { CourseSection } from "./CourseSection"
+import { DeadlineSection } from "./DeadlineSection"
+import { ResourceSection } from "./ResourceSection"
 
 export function Overview() {
   return (
@@ -29,6 +34,39 @@ export function Overview() {
       </Card>
 
       {/* Add more dashboard cards */}
+      {/* Dashboard Content Grid */}
+        <div className="p-6 grid gap-6">
+          {/* Welcome Banner */}
+          <WelcomeBanner />
+
+          {/* Progress Overview */}
+          <div className="grid md:grid-cols-3 gap-6">
+            <ProgressCard 
+              title="Course Progress" 
+              value="78%" 
+              color="primary" 
+            />
+            <ProgressCard 
+              title="Assignments Completed" 
+              value="12/15" 
+              color="secondary" 
+            />
+            <ProgressCard 
+              title="Streak Days" 
+              value="7" 
+              color="success" 
+            />
+          </div>
+
+          {/* Current Courses */}
+          <CourseSection />
+
+          {/* Upcoming Deadlines */}
+          <DeadlineSection />
+
+          {/* Learning Resources */}
+          <ResourceSection />
+        </div>
     </div>
   )
 }
